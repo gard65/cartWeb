99f6c79c95b4086c7d5132dc8687cdbcc669f771
Anton <anton_st_01_02@mail.ru>
2022-04-29 17:39:13 +0500

    server DB

---
+++ server/db/models/route.js
@@ -0,0 +1,32 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Route extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsToMany(models.User, {
+        through: 'UserRoute',
+        foreignKey: 'userId',
+      });
+      this.hasMany(models.Chat, { foreignKey: 'routeId' });
+      this.hasOne(models.Review, { foreignKey: 'routeId' });
+    }
+  }
+  Route.init({
+    time: DataTypes.TIME,
+    date: DataTypes.DATE,
+    pointA: DataTypes.TEXT,
+    pointB: DataTypes.TEXT,
+  }, {
+    sequelize,
+    modelName: 'Route',
+  });
+  return Route;
+};
---
+++ server/db/models/user.js
@@ -0,0 +1,36 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.hasOne(models.Documentation, { foreignKey: 'userId' });
+      this.hasOne(models.License, { foreignKey: 'userId' });
+      this.hasOne(models.Avatar, { foreignKey: 'userId' });
+      this.hasMany(models.Chat, { foreignKey: 'userId' });
+      this.belongsToMany(models.Route, {
+        through: 'UserRoute',
+        foreignKey: 'routeId',
+      });
+      this.hasOne(models.Chat, { foreignKey: 'userId' });
+    }
+  }
+  User.init({
+    name: DataTypes.STRING,
+    telephone: DataTypes.STRING,
+    password: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
