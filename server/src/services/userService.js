3418c58909bb85c4c6abe749034d85aecb368bdd
Alex <alexshilikhin@yandex.ru>
2022-05-12 12:14:42 +0300

    editInfo FIX 12:14

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -7,9 +7,9 @@ const {
 
 class UserService {
   async registration(name, email, telephone, password, gender, age) {
-    console.log('==========> reg');
+
     const candidate = await User.findOne({ where: { email } });
-console.log([candidate])
+  
     if (candidate) {
       throw ApiError.BadRequest(`Пользователь с почтовым адресом ${email} уже существует`);
     }
@@ -17,11 +17,11 @@ console.log([candidate])
     const user = await User.create({
       name, email, telephone, age, gender, password: hashPassword,
     });
-  
+
     const userDto = {
       email: user.email, id: user.id,
     };
-    
+
     const tokens = await tokenService.generateToken({ ...userDto });
 
     await tokenService.saveToken(userDto.id, tokens.refreshToken);
@@ -29,7 +29,6 @@ console.log([candidate])
   }
 
   async login(email, password) {
-    console.log('=======> login');
     const user = await User.findOne({
       where: { email },
       include: [
@@ -38,11 +37,10 @@ console.log([candidate])
       ],
     });
 
-    console.log('======USER=====', user);
+
 
     const userPars = JSON.parse(JSON.stringify(user));
 
-    console.log('======USERPARS=====', userPars);
     if (!user) {
       throw ApiError.BadRequest('Пользователь с таким email не найден');
     }
@@ -59,12 +57,10 @@ console.log([candidate])
       avtoNum: userPars.Driver?.avto,
     };
 
-    console.log('=====USER-DTO===', userDto);
     const tokens = await tokenService.generateToken({ ...userDto });
 
-    console.log('=====TOKEN===', tokens);
-    const tekServ = await tokenService.saveToken(userDto.id, tokens.refreshToken);
-    console.log("======TEKSERV", tekServ);
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
     return { ...tokens, user: userDto };
   }
 
